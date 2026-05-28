f5fb3e0fdbb0dbf6bd2dd21952be87e6984452e0
𝗦𝗣𝗬-𝗘 <161654922+123tool@users.noreply.github.com>
2026-05-29 03:13:58 +0700

    Create renderer.py

---
+++ core/renderer.py
@@ -0,0 +1,16 @@
+from rich.panel import Panel
+from rich.text import Text
+import pyfiglet
+import datetime
+import json
+
+class Renderer:
+    def __init__(self):
+        with open('config.json', 'r') as f: self.config = json.load(f)
+
+    def render_display(self, content_text, title="ZENITH"):
+        return Panel(
+            Text(content_text, style=self.config['theme_color']),
+            title=f"[ {title} ]",
+            border_style=self.config['theme_color']
+        )
