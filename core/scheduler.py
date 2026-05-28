056ebebb967c7fe7416da02a4e1f13c3edef557a
𝗦𝗣𝗬-𝗘 <161654922+123tool@users.noreply.github.com>
2026-05-29 03:26:47 +0700

    Update scheduler.py

--- core/scheduler.py
+++ core/scheduler.py
@@ -1,21 +1,14 @@
-import threading
-import time
-import datetime
+import threading, time, datetime
 from utils.sound import play_alert
-
 class AlarmScheduler:
     def __init__(self, db):
         self.db = db
-        self._running = True
-
     def _monitor(self):
-        while self._running:
-            alarms = self.db.get_alarms()
+        while True:
             now = datetime.datetime.now().strftime("%H:%M")
-            if now in alarms:
+            if now in self.db.get_alarms():
                 play_alert()
                 time.sleep(60)
             time.sleep(10)
-
     def start(self):
         threading.Thread(target=self._monitor, daemon=True).start()
