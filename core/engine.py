50540cec9cc86eac652ae79cad5c6b9441247851
𝗦𝗣𝗬-𝗘 <161654922+123tool@users.noreply.github.com>
2026-05-29 03:25:56 +0700

    Update engine.py

--- core/engine.py
+++ core/engine.py
@@ -1,8 +1,13 @@
 import time
-
 class TimeEngine:
     @staticmethod
     def format_duration(seconds):
         mins, secs = divmod(int(seconds), 60)
         hours, mins = divmod(mins, 60)
         return f"{hours:02}:{mins:02}:{secs:02}"
+    @staticmethod
+    def countdown(seconds):
+        while seconds >= 0:
+            yield seconds
+            time.sleep(1)
+            seconds -= 1
